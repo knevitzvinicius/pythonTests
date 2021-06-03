74c4a116e6753061f61d2fe761b3f90ac5a72588
knevitzvinicius <knevitzvinicius@gmail.com>
2021-06-02 23:10:55 -0300

    Adicionei mes por extenso

--- calculaIdade.py
+++ calculaIdade.py
@@ -1,11 +1,14 @@
+def converteMes(mesNumero):
+    mesesLongo = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+    mesLongo = mesesLongo[int(mesNumero) - 1]
+    return mesLongo
+
 dia, mes, ano = input("Digite sua data de nascimento (DD/MM/YYYY):\n").strip().split("/")
 idade = 2021 - int(ano)
+mes = converteMes(mes)
 if dia == "1":
     print(f"Você nasceu em 1º de {mes} de {ano} e tem {idade} anos")
 else:
-    print(f"Você nasceu no dia {dia} de {mês} de {ano} e tem {idade} anos")
+    print(f"Você nasceu no dia {dia} de {mes} de {ano} e tem {idade} anos")
 
-#def converteMes(mesNumero)
-#    mesesLongo = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
-#    mesLongo = mesesLongo[mesNumero]
-#    return mesLongo
+
